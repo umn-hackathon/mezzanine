a0ef094c1fb66744a899ef86b744a3a78dbf667d
alviandk <alviandk@gmail.com>
2016-11-27 06:58:34 +0700

    notif

--- comment_filter/urls.py
+++ comment_filter/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-from  comment_filter.views import censored_check, report_comment
+from  comment_filter.views import censored_check, report_comment, notifications, notifications_mark_all_as_read
 
 
 urlpatterns =[
     url(r'^censored_check/$', censored_check, {}, 'censored_check'),
     url(r'^report_comment/$', report_comment, {}, 'report_comment'),
+    url(r'^notifications/$', notifications, {'template_name': 'notifications.html'}, 'notifications'),
+    url(r'^notifications_mark_all_as_read/$', notifications_mark_all_as_read, {}, 'notifications_mark_all_as_read'),
+
 ]
